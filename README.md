56e3350aec1a92ceacd74cc42f46ee850a49d202
NMan1 <nickmantini01@gmail.com>
2020-03-30 15:14:42 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Genius Lyric Scrapper + API 
+### Find any lyrics to any song only given song name or optionally plus artist. Combines Genius's api and beautiful soup web scraping
+
+## This could also be used for genereal purpose song data collection etc. You get a lot more info than just the song lyrics in the data dictonary.
+
+### Pictures
+
+With artist
+<img src="https://i.imgur.com/ytpNdYi.png"/>
+
+Without artist
+<img src="https://i.imgur.com/O76B6m2.png"/>
---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+from urllib.request import Request
+
+
+api_key = 'amh8giT5uiMRuUSvvs6C4Tktyr_XQCOT6XK5qdV_pgShCuWX9N7O0GQXJe4fNXsq'
+
+
+def make_request(song, artist):
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + api_key}
+    search_url = base_url + '/search'
+    data = {'q': song + ' ' + artist}
+    response = requests.get(search_url, data=data, headers=headers)
+    return response.json()
+
+
+if __name__ == "__main__":
+    song = input("Enter song name: ")
+    artist = input("(Optional) Enter artist: ")
+
+    if artist:
+        print(f"Given: {song} | {artist}")
+        json = make_request(song, artist)
+    else:
+        print(f"\nGiven: {song}")
+        json = make_request(song, "")
+
+    hit_data = []
+
+    for hit in json['response']['hits']:
+        hit = hit['result']
+        hit_data.append({'artist': hit['primary_artist']['name'], 'song': hit['full_title'].replace("\xa0", " "), 'song_url': hit['url'], 'artist_url': hit['primary_artist']['url']})
+
+    print(f"Found: {hit_data[0]['song']} by {hit_data[0]['artist']}")
+
+    req = Request(hit_data[0]['song_url'], headers={'User-Agent': 'Mozilla/5.0'})
+    html = uReq(req).read()
+    page_soup = soup(html, "html.parser")
+
+    print(page_soup.find('div', {'class': 'lyrics'}).get_text()[1:])
+
+
+
